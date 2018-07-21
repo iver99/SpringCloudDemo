9e48c30dcdfce4b1c9533d2dfe9416a743ef5338
chehao <chendong.h.hao@oracle.com>
2018-07-21 18:02:42 +0800

    config

--- src/main/java/test/ConfigApplication.java
+++ src/main/java/test/ConfigApplication.java
@@ -1,3 +1,5 @@
+package test;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
